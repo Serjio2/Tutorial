d32b2af82096f700715d4cd9f91e1b9896f9419e
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-06-20 23:46:24 +0300

    ++

---
+++ js/index.js
@@ -0,0 +1,116 @@
+// // Will run first
+// console.log("First log");
+
+// setTimeout(() => {
+//   // Will run last, after 2000 milliseconds
+//   console.log("Second log");
+// }, 2000);
+
+// // Will run second
+// console.log("Third log");
+
+// const button = document.querySelector("button");
+
+// const onClick = () => {
+//   setTimeout(() => {
+//     alert("I love async JS!");
+//   }, 2000);
+// };
+
+// // Two seconds after clicking the button, 
+// // alert specified inside the setTimeout callback will appear.
+// button.addEventListener("click", onClick);
+
+// const greet = () => {
+//     console.log("Hello!");
+//   };
+  
+//   const timerId = setTimeout(greet, 3000);
+  
+//   clearTimeout(timerId);
+
+// const startBtn = document.querySelector(".js-start");
+// const stopBtn = document.querySelector(".js-stop");
+// let timerId = null;
+
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
+
+// const date = new Date();
+
+// console.log(date);
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(date.toString());
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(new Date(0));
+// // "Thu Jan 01 1970 03:00:00 GMT+0300 (Eastern European Standard Time)"
+
+// console.log(new Date(15000));
+// // "Thu Jan 01 1970 03:00:15 GMT+0300 (Eastern European Standard Time)"
+
+// console.log(date.getTime()); // 1624021654154
+
+// const date = new Date();
+// console.log("Date: ", date);
+
+// // Повертає день місяця від 1 до 31
+// console.log("getDate(): ", date.getDate());
+
+// // Повертає день тижня від 0 до 6
+// console.log("getDay(): ", date.getDay());
+
+// // Повертає місяць від 0 до 11
+// console.log("getMonth(): ", date.getMonth());
+
+// // Повертає рік з 4 цифр
+// console.log("getFullYear(): ", date.getFullYear());
+
+// // Повертає години
+// console.log("getHours(): ", date.getHours());
+
+// // Повертає хвилини
+// console.log("getMinutes(): ", date.getMinutes());
+
+// // Повертає секунди
+// console.log("getSeconds(): ", date.getSeconds());
+
+// // Повертає мілісекунди
+// console.log("getMilliseconds(): ", date.getMilliseconds());
+
+// // const date = new Date();
+// console.log("Date: ", date);
+
+// // Повертає день місяця від 1 до 31
+// console.log("getUTCDate(): ", date.getUTCDate());
+
+// // Повертає день тижня від 0 до 6
+// console.log("getUTCDay(): ", date.getUTCDay());
+
+// // Повертає місяць від 0 до 11
+// console.log("getUTCMonth(): ", date.getUTCMonth());
+
+// // Повертає рік з 4 цифр
+// console.log("getUTCFullYear(): ", date.getUTCFullYear());
+
+// // Повертає години
+// console.log("getUTCHours(): ", date.getUTCHours());
+
+// // Повертає хвилини
+// console.log("getUTCMinutes(): ", date.getUTCMinutes());
+
+// // Повертає секунди
+// console.log("getUTCSeconds(): ", date.getUTCSeconds());
+
+// // Повертає мілісекунди
+// console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
